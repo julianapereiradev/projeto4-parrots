2e8c4ebfea1348672f7bbd2c206c1f9a21fd930a
juliana <julianapereira.dev@gmail.com>
2023-04-06 08:08:45 -0300

    Consegui colocar prompt para renderizar somente a quantidade de cartas que eu quero (falta: conseguir colocar a imagem aleatória de acordo com a quantidade par de cartas que eu tenho)

---
+++ script.js
@@ -0,0 +1,21 @@
+let contador = 0;
+let jogar = document.querySelector('.principal');
+let qtdeCartas = Number(prompt('Com quantas cartas quer jogar?'));
+
+while (qtdeCartas < 4 || qtdeCartas > 14 || qtdeCartas % 2 !== 0) {
+  qtdeCartas = Number(prompt('Com quantas cartas quer jogar?'));
+}
+
+while (contador < qtdeCartas) {
+  jogar.innerHTML =
+    jogar.innerHTML +
+    `<div class="card" data-test="card">
+        <div class="front-face face">
+            <img class="parrot-virado-baixo" src="./images//back.png" data-test="face-down-image" >
+        </div>
+        <div class="back-face face">
+            Desvirou
+        </div>
+    </div>`;
+  contador++;
+}
