a060291988ccf413f4d0666ba995e8483cb2e0bc
juliana <julianapereira.dev@gmail.com>
2023-04-08 16:55:51 -0300

    Troquei array por divs estáticas e if's

--- script.js
+++ script.js
@@ -1,62 +1,91 @@
-const arrayCartas = [
-  'images/bobrossparrot.gif',
-  'images/explodyparrot.gif',
-  'images/fiestaparrot.gif',
-  'images/metalparrot.gif',
-  'images/revertitparrot.gif',
-  'images/tripletsparrot.gif',
-  'images/unicornparrot.gif',
-];
+let arraySelecionado = []
 
-const arraySelecionado = [];
+let cards = document.querySelectorAll(".memory-card")
 
-let contador = 0;
+let jogadas = 0
 
-let cartaCriada = '';
+let primeiraCarta, segundaCarta
 
-function quantidadeCartas() {
-  let qtdeCartas = prompt('Digite a quantidade de cartas que você quer jogar');
+let carta_
 
-  while (qtdeCartas < 4 || qtdeCartas > 14 || qtdeCartas % 2 !== 0) {
-    qtdeCartas = Number(
-      prompt(
-        'Com quantas cartas quer jogar? Só pode valores pares entre 4 e 14'
-      )
-    );
-  }
+let qtdcarta = prompt("Digite a quantidade de cartas que você quer jogar (4 a 14)")
+qtdcarta = Number(qtdcarta)
 
-  contador = 0;
-  while (qtdeCartas / 2 > contador) {
-    arraySelecionado.push(`${arrayCartas[contador]}`);
-    arraySelecionado.push(`${arrayCartas[contador]}`);
-    contador++;
-  }
-
-  let lista = document.querySelector('.div-pai');
-
-  contador = 0;
-  while (qtdeCartas > contador) {
-    lista.innerHTML =
-      lista.innerHTML +
-      `<div class="card" data-test="card">
-            <div class="front-face face">
-                <img class="parrot-virado-baixo" src="./images//back.png" data-test="face-down-image"/>
-            </div>
-            <div class="back-face face">
-                <img class="parrot-virado-cima" src="./${arraySelecionado[contador]}" />
-            </div>
-        </div>`;
-    contador++;
-    console.log('arraySelecionado antes', arraySelecionado);
-  }
-
-  console.log('qtdeCartas', qtdeCartas);
-  console.log('lista', lista);
-  console.log('lista.innerHTML', lista.innerHTML);
-  console.log('arrayCartas', arrayCartas);
-  console.log('arrayCartas[contador]', arrayCartas[contador]);
-  console.log('arrayCartas.length', arrayCartas.length);
-  console.log('arraySelecionado depoiss', arraySelecionado);
-  console.log('contador', contador);
+while ((qtdcarta % 2 !== 0) ||
+    (qtdcarta < 4) || (qtdcarta > 14)) {
+    qtdcarta = prompt("Com quantas cartas quer jogar? Só pode valores pares entre 4 e 14")
+    qtdcarta = Number(qtdcarta)
 }
-quantidadeCartas();
+
+for (let i = 0; i < qtdcarta; i++) {
+    arraySelecionado.push(cards[i])
+    // console.log('cards[i]', cards[i])
+    console.log('arrayseleconado', arraySelecionado.push(cards[i]))
+}
+
+arraySelecionado.forEach(card => {
+    card.style.order = Math.floor(Math.random() * 14);;
+    card.setAttribute("onclick", "clicandoCartas(this)")
+    card.classList.remove("escondido")
+    // console.log('card depois', card)
+    // console.log('arrayseleconado dpsss',arraySelecionado[0])
+})
+
+
+function clicandoCartas(selector) {
+    const virandoCartas = document.querySelectorAll(".card-flipped")
+    if (virandoCartas.length < 2) {
+        if (primeiraCarta == undefined) {
+            primeiraCarta = selector
+            primeiraCarta.removeAttribute("onclick")
+        } else if (primeiraCarta != selector) {
+            segundaCarta = selector
+            primeiraCarta.removeAttribute("onclick")
+        }
+        if (primeiraCarta != undefined && segundaCarta != undefined) {
+
+            if (primeiraCarta.id == segundaCarta.id) {
+                primeiraCarta.classList.add("estat")
+                segundaCarta.classList.add("estat")
+
+                const esticCartas = document.querySelectorAll(".estat")
+                primeiraCarta.classList.remove("card-flipped")
+                segundaCarta.classList.remove("card-flipped")
+                primeiraCarta = undefined
+                segundaCarta = undefined
+                if (esticCartas.length == qtdcarta) {
+                    setTimeout(() => {
+                        alert(`Você ganhou em ${jogadas/2} jogadas!`)
+                    }, 500)
+
+                }
+
+            } else {
+                selector.classList.add("card-flipped")
+                if (segundaCarta != undefined) {
+                    setTimeout(() => {
+                        primeiraCarta.classList.remove("card-flipped")
+                        segundaCarta.classList.remove("card-flipped")
+                        primeiraCarta.setAttribute("onclick", "clicandoCartas(this)")
+                        segundaCarta.setAttribute("onclick", "clicandoCartas(this)")
+                        primeiraCarta = undefined
+                        segundaCarta = undefined
+                    }, 1000)
+                }
+            }
+        } else {
+            selector.classList.add("card-flipped")
+            if (segundaCarta != undefined) {
+                setTimeout(() => {
+                    primeiraCarta.classList.remove("card-flipped")
+                    segundaCarta.classList.remove("card-flipped")
+                    primeiraCarta.setAttribute("onclick", "clicandoCartas(this)")
+                    segundaCarta.setAttribute("onclick", "clicandoCartas(this)")
+                    primeiraCarta = undefined
+                    segundaCarta = undefined
+                }, 1000)
+            }
+        }
+    }
+    jogadas = jogadas + 1
+}
